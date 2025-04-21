f86f19dd1d4e914dcfd4e9bdd9b2edf228cfbdc9
AdrianOficial <120221652+The-DiosBot-MD@users.noreply.github.com>
2025-04-21 14:39:35 -0400

    The-MikuBot-MD

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
+export const config = {
+    tokens: {
+        telegram: process.env.TELEGRAM_BOT_TOKEN,
+        discord: process.env.DISCORD_BOT_TOKEN,
+    },
+    credentials: {
+        instagram: {
+            username: process.env.INSTAGRAM_USERNAME,
+            password: process.env.INSTAGRAM_PASSWORD,
+        },
+        messenger: {
+            accessToken: process.env.MESSENGER_BOT_TOKEN,
+        },
+        threads: {
+            username: process.env.THREADS_USERNAME,
+            password: process.env.THREADS_PASSWORD,
+        },
+    },
+};
