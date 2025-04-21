c17189a54446b05ecce36d57e412b890f9d1926a
AdrianOficial <120221652+The-DiosBot-MD@users.noreply.github.com>
2025-04-21 14:42:26 -0400

    The-MikuBot-MD

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,12 @@
+import chalk from 'chalk'
 
+export const logger={  
+telegram : chalk.hex('#0088cc'),  
+discord : chalk.hex('#af19fa'),  
+whatsapp : chalk.hex('#25d366'),  
+instagram : chalk.hex('#E1306C'),  
+messenger : chalk.hex('#00B2FF'),  
+threads : chalk.hex('#000000'),  
+info : chalk.bold ,  
+error : chalk.red  
+}
