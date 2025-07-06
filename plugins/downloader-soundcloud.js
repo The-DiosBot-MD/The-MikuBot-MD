8c4d43b5ae86fa9f9db3f444c7cc2462a8ab7809
Wichi <sebastianbarbaro82@gmail.com>
2025-07-05 23:00:55 -0400

    Actualizar downloader-soundcloud.js

--- plugins/downloader-soundcloud.js
+++ plugins/downloader-soundcloud.js
@@ -59,7 +59,7 @@ let handler = async (m, { conn, args, command, usedPrefix}) => {
 
 handler.help = ['play','play1'];
 handler.tags = ['descargas'];
-handler.command = /^play1$/i;
+handler.command = ['play',play2']
 handler.register = true;
 
 export default handler;
