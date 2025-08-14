e191de62f9d574099cdc251560411287652c6851
Carlos <supernumerovirtual@gmail.com>
2025-08-14 13:53:39 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -264,7 +264,7 @@ conn: this,
                 let _args = noPrefix.trim().split` `.slice(1)
                 let text = _args.join` `  
 command = (command || '').toLowerCase()  
-const groupLimitado = '120363160004045293@g.us'  
+const groupLimitado = ''  
 const comandosPermitidos = ['serbot', 'bots', 'kick', 'code', 's', 'delsession', 'on', 'off', 'tutosub', 'menu', 'estado', 'play', 'sistema'];  
 
 if (m.chat === groupLimitado && !comandosPermitidos.includes(command)) {
