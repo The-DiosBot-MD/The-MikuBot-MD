e31a56f2fcce9d1773d403206b70df96af101b53
Carlos <supernumerovirtual@gmail.com>
2025-08-21 20:05:31 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -264,7 +264,7 @@ conn: this,
                 let _args = noPrefix.trim().split` `.slice(1)
                 let text = _args.join` `  
 command = (command || '').toLowerCase()  
-const groupLimitado = '120363160004045293@g.us'  
+const groupLimitado = '120363400854207202@g.us'  
 const comandosPermitidos = ['serbot', 'bots', 'kick', 'code', 's', 'delsession', 'on', 'off', 'tutosub', 'menu', 'estado', 'update', 'sistema'];  
 
 if (m.chat === groupLimitado && !comandosPermitidos.includes(command)) {
