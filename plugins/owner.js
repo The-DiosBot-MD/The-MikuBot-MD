e304aa7bcb7d61e1b88bda8c4c8ef6fb6938d79d
Carlos <supernumerovirtual@gmail.com>
2025-09-09 10:04:29 -0400

    Actualizar owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,19 +6,24 @@ import { createRequire } from 'module'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname)
-//
+
 let handler = async (m, _2) => {
   let { conn, usedPrefix, noPrefix, args, groupMetadata } = _2
+
+  
+  if (m.fromMe || m.sender === conn.user.jid) return
+
   let _return
   let _syntax = ''
   let _text = (/^=/.test(usedPrefix) ? 'return ' : '') + noPrefix
   let old = m.exp * 1
   try {
     let i = 15
-    let f = {
-      exports: {}
-    }
-    let exec = new (async () => { }).constructor('print', 'm', 'handler', 'require', 'conn', 'Array', 'process', 'args', 'groupMetadata', 'module', 'exports', 'argument', _text)
+    let f = { exports: {} }
+    let exec = new (async () => {}).constructor(
+      'print', 'm', 'handler', 'require', 'conn', 'Array', 'process',
+      'args', 'groupMetadata', 'module', 'exports', 'argument', _text
+    )
     _return = await exec.call(conn, (...args) => {
       if (--i < 1) return
       console.log(...args)
@@ -28,7 +33,7 @@ let handler = async (m, _2) => {
     let err = syntaxerror(_text, 'Función de ejecución', {
       allowReturnOutsideFunction: true,
       allowAwaitOutsideFunction: true,
-        sourceType: 'module'
+      sourceType: 'module'
     })
     if (err) _syntax = '```' + err + '```\n\n'
     _return = e
@@ -37,6 +42,7 @@ let handler = async (m, _2) => {
     m.exp = old
   }
 }
+
 handler.help = ['>', '=>']
 handler.tags = ['advanced']
 handler.customPrefix = /^=?> /
