4e3487d706ef3fe0140d98fbbaa15968ef405895
Carlos <supernumerovirtual@gmail.com>
2025-09-16 23:02:40 -0400

    Actualizar owner-reporte.js

--- plugins/owner-reporte.js
+++ plugins/owner-reporte.js
@@ -1,29 +1,34 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
+let handler = async (m, { conn, text }) => {
     if (!text) throw '⚠️ *_Ingrese el error que desea reportar._*'
     if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
     if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
 
-    const teks = `╭───────────────────
+    const cliente = `Wa.me/${m.sender.split`@`[0]}`
+    const cita = m.quoted?.text ? `\n📝 *Cita:* ${m.quoted.text.slice(0, 300)}...` : ''
+    const mensaje = `╭───────────────────
 │⊷〘 *R E P O R T E* 🤍 〙⊷
 ├───────────────────
 │⁖🧡꙰  *Cliente:*
-│✏️ Wa.me/${m.sender.split`@`[0]}
+│✏️ ${cliente}
 │
 │⁖💚꙰  *Mensaje:*
-│📩 ${text}
+│📩 ${text}${cita}
 ╰───────────────────`
 
-    const citado = m.quoted?.text ? `\n📝 *Cita:* ${m.quoted.text.slice(0, 300)}...` : ''
-    const mensajeFinal = teks + citado
-
-    // Enviar al dueño
-    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', mensajeFinal, m, { mentions: conn.parseMention(mensajeFinal) })
-
-    // Enviar al grupo de soporte 
-    await conn.reply('120363421252082747@g.us', mensajeFinal, m, { mentions: conn.parseMention(mensajeFinal) })
+    
+    const destinatarios = [
+        '595976126756@s.whatsapp.net',      // Dueño
+        '5355699866@s.whatsapp.net',        // Mods
+        '120363421252082747@g.us'           // grupo de soporte 
+    ]
 
     
-    m.reply('🧸 Gracias por confiar en *Miku-Bot*. Tu reporte fue enviado al staff, será arreglado lo antes posible.')
+    for (let jid of destinatarios) {
+        await conn.reply(jid, mensaje, m)
+    }
+
+   
+    m.reply('🧸 Gracias por confiar en *Miku-Bot*. Tu reporte fue enviado al staff y al grupo de validación. Será atendido lo antes posible.')
 }
 
 handler.help = ['reportar']
