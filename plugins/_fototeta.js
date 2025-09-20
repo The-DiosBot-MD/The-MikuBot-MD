38344452cfad8932a6bdc0c9ab3a49c026924cba
Carlos <supernumerovirtual@gmail.com>
2025-09-20 14:05:54 -0400

    Actualizar _fototeta.js

--- plugins/_fototeta.js
+++ plugins/_fototeta.js
@@ -1,11 +1,15 @@
 const handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, {
-    text: `*Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. Y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.*`
-  }, { quoted: m })
+  await conn.sendMessage(
+    m.chat,
+    {
+      text: `*Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. Y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.*`
+    },
+    { quoted: m }
+  )
 }
 
-handler.command = /^fototeta$/i   
-handler.tags = ['fun']
 handler.help = ['fototeta']
+handler.tags = ['fun']
+handler.command = /^fototeta$/i 
 
 export default handler
