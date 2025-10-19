e77f74fd91128083f3b06ead941e44fa0b38c835
Carlos <supernumerovirtual@gmail.com>
2025-10-18 23:31:07 -0400

    Actualizar owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -43,9 +43,9 @@ let handler = async (m, _2) => {
   }
 }
 
-handler.help = ['>', '=>']
+handler.help = ['=>']
 handler.tags = ['advanced']
-handler.customPrefix = /^=?> /
+handler.customPrefix = /^=? /
 handler.command = /(?:)/i
 
 export default handler
